07662fbf4b600dbd243f732e2d766fdee0db4592
jkolodziej <julkaa.k@gmail.com>
2021-03-28 22:16:37 +0200

    Add fellowship assertion test

--- unit_testing_2021/src/test/java/pl/lodz/p/zzpj/testing/assertj/FellowshipAssertionTest.java
+++ unit_testing_2021/src/test/java/pl/lodz/p/zzpj/testing/assertj/FellowshipAssertionTest.java
@@ -3,8 +3,9 @@ package pl.lodz.p.zzpj.testing.assertj;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static pl.lodz.p.zzpj.testing.assertj.FellowTestFixture.frodo;
-import static pl.lodz.p.zzpj.testing.assertj.FellowTestFixture.sauron;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static pl.lodz.p.zzpj.testing.assertj.Fellow.Race.*;
+import static pl.lodz.p.zzpj.testing.assertj.FellowTestFixture.*;
 
 public class FellowshipAssertionTest {
 
@@ -12,47 +13,55 @@ public class FellowshipAssertionTest {
 
     @Test
     public void frodoShouldBeIn() {
-
+        assertThat(fellowship).contains(frodo());
     }
 
     @Test
     public void sauronShouldNotBeIn() {
-
+        assertThat(fellowship).doesNotContain(sauron());
     }
 
     @Test
     public void shouldFrodoNameBeCorrectAndNotBeASauronAndBeInFellowship() {
-
+        assertThat(fellowship).extracting("name").contains("Frodo").doesNotContain("Sauron");
     }
 
     @Test
     public void aragornShouldBeBeforeBoromir() {
+        assertThat(fellowship).containsSubsequence(aragorn(), boromir());
     }
 
     @Test
     public void shouldNotContainDuplicatedFellows() {
+        assertThat(fellowship).doesNotHaveDuplicates();
     }
 
     @Test
     public void shouldContainOnlyFellowsWithExpectedNamesInProperOrder() {
+        assertThat(fellowship).extracting("name")
+                .containsSequence("Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Legolas", "Gimli", "Aragorn", "Boromir");
     }
 
     @Test
     public void shouldContainNineFellowsButNoneGiant() {
+        assertThat(fellowship).hasSize(9).filteredOn("race", GIANT).isEmpty();
     }
 
     @Test
     public void shouldContainTupleForBoromirSamAndLegolas() {
         // Extracting multiple values at once (using tuple to group them)
         // Create tuples with name and race name
+        assertThat(fellowship).extracting("name", "race")
+                .contains(tuple("Boromir", MAN),
+                          tuple("Sam", HOBBIT),
+                          tuple("Legolas", ELF));
     }
 
 
     @Test
     public void shouldContainsFourHobbits() {
-
+        assertThat(fellowship).filteredOn("race", HOBBIT).hasSize(4);
     }
-
 
     private Fellowship formTheFellowshipOfTheRing() {
         return new Fellowship(
@@ -63,7 +72,7 @@ public class FellowshipAssertionTest {
                 FellowTestFixture.gandalf(),
                 FellowTestFixture.legolas(),
                 FellowTestFixture.gimli(),
-                FellowTestFixture.aragorn(),
+                aragorn(),
                 FellowTestFixture.boromir());
     }
 }
